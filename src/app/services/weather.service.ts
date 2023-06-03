4fa7ab8ae027385bd1bd9da0f0be117d4357fb72
Usuario <britezxavierluca@gmail.com>
2023-06-02 21:14:30 -0300

    modified:   src/app/home/home.page.html 	modified:   src/app/home/home.page.scss 	modified:   src/app/services/weather.service.ts 	modified:   src/index.html

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,10 +12,8 @@ export class WeatherService{
 
     getWeather(city: string): Observable<any> {
         const apiKey = '6654885ebf4e60d58851987a48a3981b';
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-        
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`;
         return from(axios.get(url));
-        
     }
 
       
